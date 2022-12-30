69c9fab0534c89f3e4efb0b028b2e7ea7fcb1c06
kontarevsy <119353766+kontarevsy@users.noreply.github.com>
2022-12-30 17:10:21 +0300

    Add files via upload

---
+++ Program.cs. Ex52.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static int Main(string[] args)
+    {// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце. Например задан массив:
+     //1 4 7 2
+     //5 9 2 3 
+     //8 4 2 3
+     //Среднее арифметическое каждого столбца:
+/ 4,6; 5,6; 3,6 3
+
+Random random = new Random();
+        int[,] array = new int[random.Next(1, 10), random.Next(1, 10)];
+        for (int i = 0, i < array.GetLength(0), i++) ;
+
+        for (int j = 0, j < array.GetLength(1), j++) ;
+        array[i, j] = random.Next(1, 10);
+        Console.WriteLine();
+        Console.WriteLine("----");
+        Console.WriteLine(array.GetLength(0); i++);
+        for (int j = 0, j< array.GetLength(0); i++) ;
+        sum = array[i, j];
+        Console.Write($ { sum / array.GetLength(0)});
+        ");
+Console.ReadLine;
+    }
+}
